9f74f92ed628ed6c073899ae238a73816e95f05a
nyangkosense <sebastian.michalk@protonmail.com>
2024-10-25 20:59:41 +0000

    column patch added

--- config.h
+++ config.h
@@ -57,6 +57,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "@|@",      snail },
+	{ "||",       col },
 };
 
 /* monitors */
@@ -188,6 +189,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_s,          setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                    XKB_KEY_c,          setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
 	{ MODKEY,                    XKB_KEY_e,         togglefullscreen, {0} },
